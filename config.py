0d6a8deb1dd68af8a17a3f57fce0a17cae6c71e0
Samira Bourgi GUEYE <samira-bourgi.gueye@groupe3f.fr>
2026-07-07 20:18:46 +0200

    Use Streamlit secrets for database URL

--- config.py
+++ config.py
@@ -1,7 +1,14 @@
-CLIENT_ID = "c0ef2840-8322-4482-85ba-0641b90e63fd"
-CLIENT_SECRET = "a383254f-2af4-4bf1-931c-badd63ac1c16"
+# Code pour connexion avec Postgresql
 
-TOKEN_URL = "https://accounts.hubintent.com/oauth/token"
-BASE_URL = "https://api.hubintent.com/api"
+# CLIENT_ID = "c0ef2840-8322-4482-85ba-0641b90e63fd"
+# CLIENT_SECRET = "a383254f-2af4-4bf1-931c-badd63ac1c16"
 
-DB_URL = "postgresql+psycopg2://postgres:momopopolove2026?@localhost:5432/patrimoine"
+# TOKEN_URL = "https://accounts.hubintent.com/oauth/token"
+# BASE_URL = "https://api.hubintent.com/api"
+
+# DB_URL = "postgresql+psycopg2://postgres:momopopolove2026?@localhost:5432/patrimoine"
+
+
+import streamlit as st
+
+DB_URL = st.secrets["database"]["postgresql://postgres.ppjnxrqauxiyoyunaobf:momopopolove2026?@aws-0-eu-west-1.pooler.supabase.com:5432/postgres"]
