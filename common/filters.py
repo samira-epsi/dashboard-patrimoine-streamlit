2cad0e0f41637635672688e4afc513e9df23a269
Samira Bourgi GUEYE <samira-bourgi.gueye@groupe3f.fr>
2026-07-23 10:26:57 +0200

    Réorganise les fichiers et améliore la vue globale

--- common/filters.py
+++ common/filters.py
@@ -569,7 +569,7 @@ def inject_filters_style():
         .filters-header {
             margin-top: 0 !important;
             margin-bottom: 18px !important;
-            min-height: 150px !important;
+            min-height: 140px !important;
             box-sizing: border-box !important;
 
             display: flex !important;
