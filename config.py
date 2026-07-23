0e43cc783aead8ea00890d29a29ef623502f7f53
Samira Bourgi GUEYE <samira-bourgi.gueye@groupe3f.fr>
2026-07-23 10:49:35 +0200

    Réorganise les fichiers et améliore la vue globale

--- config.py
+++ config.py
@@ -1,14 +1,14 @@
 # Code pour connexion avec Postgresql
 
-CLIENT_ID = "c0ef2840-8322-4482-85ba-0641b90e63fd"
-CLIENT_SECRET = "a383254f-2af4-4bf1-931c-badd63ac1c16"
+# CLIENT_ID = "c0ef2840-8322-4482-85ba-0641b90e63fd"
+# CLIENT_SECRET = "a383254f-2af4-4bf1-931c-badd63ac1c16"
 
-TOKEN_URL = "https://accounts.hubintent.com/oauth/token"
-BASE_URL = "https://api.hubintent.com/api"
+# TOKEN_URL = "https://accounts.hubintent.com/oauth/token"
+# BASE_URL = "https://api.hubintent.com/api"
 
-DB_URL = "postgresql+psycopg2://postgres:momopopolove2026?@localhost:5432/patrimoine"
+# DB_URL = "postgresql+psycopg2://postgres:momopopolove2026?@localhost:5432/patrimoine"
 
 
-# import streamlit as st
+import streamlit as st
 
-# DB_URL = st.secrets["postgres"]["url"]
+DB_URL = st.secrets["postgres"]["url"]
